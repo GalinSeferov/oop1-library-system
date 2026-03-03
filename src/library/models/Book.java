f7eefc9507b5ba113b64781ddf56558c26d0f102
Galin Seferov <galinseferov@gmail.com>
2026-03-03 12:12:46 +0200

    changing genre type

--- src/library/models/Book.java
+++ src/library/models/Book.java
@@ -6,14 +6,14 @@ import java.util.ArrayList;
 public class Book {
     private String author;
     private String title;
-    private String genre;
+    private Genre genre;
     private String shortDescription;
     private int publishedYear;
     private List<String> keywords;
     private double rating;
     private String id;
 
-    public Book(String author, String title, String genre, String shortDescription, int publishedYear, List<String> keywords, double rating, String id){
+    public Book(String author, String title, Genre genre, String shortDescription, int publishedYear, List<String> keywords, double rating, String id){
 
             if(publishedYear > 2026){
                 throw new IllegalArgumentException("Year cannot be in the future!");
@@ -37,7 +37,7 @@ public class Book {
         return title;
     }
 
-    public String getGenre() {
+    public Genre getGenre() {
         return genre;
     }
 
