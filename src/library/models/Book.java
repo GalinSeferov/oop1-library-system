d88dc05f61d228c9354d80ed20d47eb13402cd54
Galin Seferov <galinseferov@gmail.com>
2026-03-03 11:48:48 +0200

    creating packages and class Application

--- src/library/models/Book.java
+++ src/library/models/Book.java
@@ -1,24 +1,34 @@
+package library.models;
+
+import java.util.List;
+import java.util.ArrayList;
+
 public class Book {
     private String author;
     private String title;
     private String genre;
     private String shortDescription;
     private int publishedYear;
-    private String keywords;
+    private List<String> keywords;
     private double rating;
     private String id;
 
-    public Book(String author, String title, String genre, String shortDescription, int publishedYear, String keywords, double rating, String id) {
+    public Book(String author, String title, String genre, String shortDescription, int publishedYear, List<String> keywords, double rating, String id){
+
+            if(publishedYear > 2026){
+                throw new IllegalArgumentException("Year cannot be in the future!");
+    }
+
         this.author = author;
         this.title = title;
         this.genre = genre;
         this.shortDescription = shortDescription;
         this.publishedYear = publishedYear;
-        this.keywords = keywords;
+        this.keywords = new ArrayList<>(keywords);
         this.rating = rating;
         this.id = id;
-    }
 
+}
     public String getAuthor() {
         return author;
     }
@@ -39,7 +49,7 @@ public class Book {
         return publishedYear;
     }
 
-    public String getKeywords() {
+    public List<String> getKeywords() {
         return keywords;
     }
 
