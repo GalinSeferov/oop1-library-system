837255727a75bee401f98198718cb9e6936ad982
Galin Seferov <galinseferov@gmail.com>
2026-03-12 15:31:23 +0200

    CLI and admin user

--- src/library/Application.java
+++ src/library/Application.java
@@ -1,4 +1,26 @@
 package library;
 
+import library.models.AccessLevel;
+import library.models.User;
+
+import java.util.Scanner;
+
 public class Application {
+    private static final User DEFAULT_ADMIN = new User("admin", "i<3Java",AccessLevel.ADMIN);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Library System Initialized.");
+
+        while (true) {
+            System.out.print("> ");
+            String input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("exit")) {
+                System.out.println("Exiting system...");
+                break;
+            }
+            //komandite za terminala
+            System.out.println("Command received: " + input);
+        }
+    }
 }
--- src/library/exceptions/NoPermission.java
+++
@@ -1,7 +0,0 @@
-package library.exceptions;
-
-public class NoPermission extends RuntimeException {
-    public NoPermission(String message) {
-        super(message);
-    }
-}
